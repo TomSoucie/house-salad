65797c80e3755584592eb9fdfac2ef95f079a0cc
Thomas Soucie <tsoucie1858@gmail.com>
2017-07-12 11:08:19 -0600

    tried JSON.parse on the curl command

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
   def index
     binding.pry
-    @members = curl "https://api.propublica.org/congress/v1/members/house/co/current.json"
-                  -H "X-API-Key: #{ENV[PROPUBLICA_API_KEY]} "
+    @members = JSON.parse(curl "https://api.propublica.org/congress/v1/members/house/co/current.json" -H "X-API-Key: ENV[#{PROPUBLICA_API_KEY}]")
   end
 end
