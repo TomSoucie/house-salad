5f65ae88a667f8abf77a3890704abe8b4bae0538
Thomas Soucie <tsoucie1858@gmail.com>
2017-07-12 10:58:37 -0600

    add curl command to search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
     binding.pry
+    @members = curl "https://api.propublica.org/congress/v1/members/house/co/current.json"
+                  -H "X-API-Key: #{ENV[PROPUBLICA_API_KEY]} "
   end
 end
