73374554f80620a6a09f1c7f820c5ace25eee30b
Thomas Soucie <tsoucie1858@gmail.com>
2017-07-12 09:46:04 -0600

    Rspec errors in setup

---
+++ spec/services/api_returns_house_members_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+=begin
+As a user
+When I visit "/"
+And I select "Colorado" from the dropdown
+And I click on "Locate Members from the House"
+Then my path should be "/search" with "state=CO" in the parameters
+And I should see a message "7 Results"
+And I should see a list of 7 the members of the house for Colorado
+And they should be ordered by seniority from most to least
+And I should see a name, role, party, and district for each member
+=end
+describe "API returns members of congress by State" do
+  context "when a user searches via the dropdown" do
+    it "searches by state" do
+      visit "/"
+      expect(response).to be_success
+
+
+    end
+  end
+end
