b4d284bb9f63e94e32b596f75eb800f1b2a81a79
Thomas Soucie <tsoucie1858@gmail.com>
2017-07-12 11:22:33 -0600

    curl command works in terminal but not familiar with how to store the response in a variable - Current search_controller action still throwing a syntax error

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
     binding.pry
-    @members = JSON.parse(curl "https://api.propublica.org/congress/v1/members/house/co/current.json" -H "X-API-Key: ENV[#{PROPUBLICA_API_KEY}]")
+    @members = curl "https://api.propublica.org/congress/v1/members/house/co/current.json" -H "X-API-Key: #{ENV[PROPUBLICA_API_KEY]}"
   end
 end
